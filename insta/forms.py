6e8b89003d555f877cf8be98d14f5e65c2886f89
sgalaano <sgalaano@gmail.com>
2019-06-24 11:16:53 +0300

    still forms

--- insta/forms.py
+++ insta/forms.py
@@ -1,3 +1,12 @@
 from django import forms
 from models import Image, Comments
 
+class NewstatusForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('image','image_caption')
+
+class NewstatusForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('comment',)
