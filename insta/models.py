014343fa86ee1e0f845dc7c1cd19dd994bb1f0b5
sgalaano <sgalaano@gmail.com>
2019-06-24 10:08:43 +0300

    added profile models

--- insta/models.py
+++ insta/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.contrib.auth.models import user 
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profiles/', null=True)
+    user_bio = models.TextField()
+    user = models.ForeignKey(user)
+    last_update = models.DateTimeField(auto_now_add=True, null=True)
+    
