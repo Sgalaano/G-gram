0375ffed1732fcb9d68c23eed9d82f85d0f8472c
sgalaano <sgalaano@gmail.com>
2019-06-24 10:14:55 +0300

    remove welcome view

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+
