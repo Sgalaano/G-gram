95ffab700f56bfd54e63984371975167cb91daab
sgalaano <sgalaano@gmail.com>
2019-06-24 10:47:07 +0300

    add profile view function

--- insta/models.py
+++ insta/models.py
@@ -17,4 +17,37 @@ class Profile(models.Model):
 
     def save_profile(self):
         self.save()
-    
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'photos/', null = True)
+    image_name = models.CharField(max_length=30, null=True)
+    image_caption = models.TextField(null =True)
+    likes = models.IntegerField(default=0)
+    date_uploaded = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(User, null=True)
+    profile = models.ForeignKey(Profile, null=True)
+
+    class Meta:
+       ordering = ['-date_uploaded']
+
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def search_by_user(cls, search_term):
+        images = cls.objects.filter(image_caption__icontains=search_term)
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, image_id):
+        images = cls.objects.get(id=image_id)
+        return images
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=300)
+    user = models.ForeignKey(User, null=True)
+    image = models.ForeignKey(Image, null=True)
+    time_comment = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+       ordering = ['-time_comment']
