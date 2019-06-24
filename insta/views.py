3505313c83cefb363fcc4664c51d9020a6b7bfe3
sgalaano <sgalaano@gmail.com>
2019-06-24 10:24:45 +0300

    add timeline request

--- insta/views.py
+++ insta/views.py
@@ -4,3 +4,11 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url=/'accounts/login')
+def timelines(request):
+    current_user = request.user 
+    images = Image.objects.order_by('-date_uploaded')
+    profiles = Profile.objects.order_by('-last_update')
+    comments = Comments.objects.order_by('-time_comments')
+    return render(request, 'timelines.html', {'images':images, 'profiles':profiles, 'user_profile':user_profile, 'comments':comments})
+'''
